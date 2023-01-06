39ea19ef7bb40128a3040ad6b985c3ea4878a9d0
mi6540bo <michail.boulasikis@cs.lth.se>
2023-01-06 15:16:52 +0100

    Small changes in comments and text.

--- main.cpp
+++ main.cpp
@@ -21,15 +21,13 @@ int main() {
     // Create the tabu algorithm configuration, with the problem, the tabu list
     // size, and the neighborhood exploration percentage.
     tabu::TabuSearch<tabu_examples::EightQueens> tabu_search{problem, options};
-    // When the algorithm starts, run this callback to get the performance of
-    // our starting point.
+    // When the algorithm starts, run this callback.
     tabu_search.set_callback_on_start(
-            [](auto x) { std::cout << "Starting search with initial solution performance 28." << std::endl; });
-    // When we find a new best solution, run this callback to get the
-    // performance of the new best solution.
+            [](const auto &x) { std::cout << "Starting search." << std::endl; });
+    // When we find a new best solution, run this callback to get the performance of the new best solution.
     tabu_search.set_callback_on_new_best(
             [](const auto &x) { std::cout << "New best solution found with cost = " << x.lowest_cost << std::endl; });
-    // Called when the algorithm randomizes the current search point.
+    // Called when the algorithm randomizes the current search point, so that we know it happened.
     tabu_search.set_callback_on_randomize([](const auto &x) { std::cout << "Randomizing search point." << std::endl; });
     // Set an early stopping callback which stops the search when the cost becomes 0 (i.e. we found a solution)
     tabu_search.set_callback_early_stopping([](const auto &x) {
@@ -39,7 +37,7 @@ int main() {
         }
         return false;
     });
-    // Run the tabu search algorithm for 100 iterations. Use an all queens on the first row initial condition.
+    // Run the tabu search algorithm for 400 iterations. Use an all queens on the first row initial condition.
     auto solution = tabu_search.run({1, 2, 3, 4, 5, 6, 7, 8}, 400);
     // Print the solution.
     std::cout << "Solution: " << std::endl;
